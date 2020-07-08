e198bc9c85ed3cb56b4391aef48ef432f9f9e8eb
Francisco <pancho.dommel@gmail.com>
2020-07-08 12:59:00 -0300

    I finish section 3 and y do personal tests.

---
+++ Course/Course/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? numero = 0;   //con el signo de interrogacion permito  que el entero sea null
+            Console.WriteLine(numero);
+
+            var claseAnonima = new { name = "francisco", surName = "Dommel", age = "23", year = "1996" };
+
+            Console.WriteLine(claseAnonima.name);
+            Console.WriteLine(claseAnonima.surName);
+            Console.WriteLine(claseAnonima.age);
+
+            string s = "1";
+            int numero1 = Convert.ToInt32(s);   // es como un parseo
+            Console.WriteLine(numero1);
+
+        }
+    }
+}
