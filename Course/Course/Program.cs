b44e5a89fd501367e83b8282100f2d4ccbf7add5
Francisco <pancho.dommel@gmail.com>
2020-07-17 10:29:14 -0300

    I finished section 5,6,7 and i did more things about inheritance in c #

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,27 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 
 namespace Course
 {
     public enum values
     {
-        value1= 1,
-        value2= 2,
-        value3= 3
+        value1 = 1,
+        value2 = 2,
+        value3 = 3
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            //int? numero = 0;   //con el signo de interrogacion permito  que el entero sea null
+            int? numero = 0;   //con el signo de interrogacion permito  que el entero sea null
             //Console.WriteLine(numero);
-            //string s = "1";
-            //int numero1 = Convert.ToInt32(s);   // es como un parseo
+            string s = "1";
+            int numero1 = Convert.ToInt32(s);   // es como un parseo
             //Console.WriteLine(numero1);
+
             Persona persona1 = new Estudiante("Gonzalo", "diaz", 22, 1997, "arquitectura");
             var claseAnonima = new { FistName = "francisco", LastName = "Dommel", age = 23, year = 1996, carrera = "tup" };
-            Persona persona2 = new Estudiante(claseAnonima.FistName, claseAnonima.LastName, claseAnonima.age,claseAnonima.year,claseAnonima.carrera);
+            Persona persona2 = new Estudiante(claseAnonima.FistName, claseAnonima.LastName, claseAnonima.age, claseAnonima.year, claseAnonima.carrera);
 
             Persona[] ArrayPersona = new Estudiante[2];
 
@@ -32,24 +34,36 @@ namespace Course
             {
                 Console.WriteLine(persona.ToString());
             }
-
+            int EdadPersona = persona1.Age;
+            Console.WriteLine(EdadPersona);
 
             string nombre = "pedro";
             string apellido = "garcia";
             string nombreCompleto = string.Format("MI nombre completo es {0} {1}", nombre, apellido);
+
             Console.WriteLine(nombreCompleto);
 
             var method = values.value1;
 
 
-        
+            //Declaro listas ==  ArrayList
+            var personas = new List<Persona>() { persona1, persona2 };
+            foreach(var p in personas)
+            {
+                Console.WriteLine("ESTO ES LISTA" + p.ToString());
+            }
 
-          
+            //Trabajo con fechas
 
+            var dateTime = new DateTime(2010,1,1);
 
+            DateTime now = DateTime.Now;
+            DateTime today = DateTime.Today;
 
+            var mañana = today.AddDays(1);
 
+            Console.WriteLine(now.ToShortDateString() + " -Mañana: - " + mañana.Month);
         }
 
-    }
+    } 
 }
---
+++ Course/Seccion5/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+
+namespace Seccion5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* EJERCICIO 1 */
+            //Console.WriteLine("(Ejercicio 1 ) Ingrese un numero.");
+            //int numero = Int32.Parse(Console.ReadLine());
+            //ejercicio1(numero);
+
+            /* EJERCICIO 2 */
+            //Console.WriteLine("(Ejercicio 2 ) Ingrese dos numeros.");
+            //int num1 = Int32.Parse(Console.ReadLine());
+            //int num2 = Int32.Parse(Console.ReadLine());
+            //ejercicio2(num1, num2);
+
+            /* EJERCICIO 3 */
+            //Console.WriteLine("(Ejercicio 3 ) Ingrese ancho y largo");
+            //int width = Int32.Parse(Console.ReadLine());
+            //int height = Int32.Parse(Console.ReadLine());
+            //ejercicio3(width, height);
+
+            /* EJERCICIO 4 */
+            //Console.WriteLine("(Ejercicio 3 ) Ingrese Speed Limit y Speed car");
+            //int speedLimit = Int32.Parse(Console.ReadLine());
+            //int speedCar = Int32.Parse(Console.ReadLine());
+            //ejercicio4(speedLimit, speedCar);
+
+
+            // * -- SEGUNDA PARTE EJERCITACIONES -- *//
+
+            /* EJERCICIO 5 */
+            //Console.WriteLine(string.Format("HAY {0} numeros divisores por 3", ejercicio5()));
+
+            /* EJERCICIO 6 */
+            // ejercicio6();
+
+            /* EJERCICIO 7 */
+            // ejercicio7();
+
+            /* EJERCICIO 8 */
+            //ejercicio8();
+
+            /* EJERCICIO 9 */
+
+
+
+        }
+        public static void ejercicio1(int numero)
+        {
+            if(numero>=1 && numero <= 10)
+                Console.WriteLine("valido");
+            else
+                Console.WriteLine("invalido");
+        }
+        public static void ejercicio2(int num1, int num2)
+        {
+            if(num1 > num2)
+                Console.WriteLine("mayor numero: " + num1);
+            else if(num2 > num1)
+                Console.WriteLine("mayor numero: " + num2);
+            else
+                Console.WriteLine( "son iguales");
+        }
+        public static void ejercicio3(int width, int height)
+        {
+            if (width > height)
+                Console.WriteLine("es un paisaje");
+            else
+                Console.WriteLine("Es un retrato");
+        }
+        public static void ejercicio4(int speedLimit, int speedCar)
+        {
+            int demeritPoin = 0, diferencia;
+            if (speedCar < speedLimit)
+            {
+                Console.WriteLine("OK");
+            } else
+            {
+                diferencia = speedCar - speedLimit;
+                demeritPoin = (diferencia / 5);
+            }
+            if(demeritPoin >= 12)
+            {
+                Console.WriteLine("Licencia suspendida");
+            }
+        }
+        public static int ejercicio5()
+        {
+            int cant = 0;
+            for(int i=0; i<100; i++)
+            {
+                if( i % 3 == 0)
+                {
+                    cant++;
+                }
+            }
+            return cant;
+        }
+        public static void ejercicio6()
+        {
+            string palabra = "";
+            int numero = 0;
+            do
+            {
+                Console.WriteLine("Ingrese numero");
+                numero += Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese ok para salir o cualquier cosa para seguir");
+                palabra = Console.ReadLine();
+            } while (!palabra.Equals("ok"));
+            Console.WriteLine("suma de todos los numeros ingresados = "+ numero);
+        }
+        public static void ejercicio7()
+        {
+            Console.WriteLine("Ingrese numero");
+            int numero = Int32.Parse(Console.ReadLine());
+            int factorial = 1;
+            for(int i=numero; i>0; i--)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine(string.Format("factorial de {0} es {1}", numero, factorial));
+
+        }
+        public static void ejercicio8()
+        {
+            Random numero = new Random();
+            int i = 0, ingresoUser=0;
+            int random = numero.Next(1, 10);
+            Console.WriteLine("numero random es:"+random);
+            while(i<4 &&  ingresoUser != random)
+            {
+                Console.WriteLine("ingresse numero para advinar");
+                ingresoUser = Int32.Parse(Console.ReadLine());
+                i++;
+            }
+            if(ingresoUser == random)
+                Console.WriteLine("encontraste el numero");
+            else
+                Console.WriteLine("perdiste");
+        }
+
+
+    }
+}
